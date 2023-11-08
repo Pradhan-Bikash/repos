819c504d8c71f06e37369697afe8dde7613aff65
BIKASH PRADHAN <bikashp2018@gmail.com>
2023-11-08 13:13:30 +0530

    Initial commit

---
+++ DemoObject/DemoObject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoObject
+{
+    internal class Student
+    {
+        int id;
+        string name;
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.id = 1;
+            s1.name = "Bikash";
+            Console.WriteLine(s1.name);
+            Console.ReadLine();
+        }
+    }
+}
