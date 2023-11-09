4932ca7222a7b5e179aa6f56c05c5178c05c3b17
BIKASH PRADHAN <bikashp2018@gmail.com>
2023-11-09 19:25:08 +0530

    form

--- firstproj/firstproj/Class1.cs
+++ firstproj/firstproj/Class1.cs
@@ -8,5 +8,6 @@ namespace firstproj
 {
     internal class Class1
     {
+
     }
 }
---
+++ form/form/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form
+{
+    public partial class mainform : Form
+    {
+        public mainform()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String messagetext = "Simple message text";
+            String messagetitle = "message title";
+            //MessageBox.Show("simple message text");
+            MessageBox.Show(messagetext,messagetitle);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
