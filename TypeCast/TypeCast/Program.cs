2b24fecf775f0b050d25384295cf07dc16b4de1f
BIKASH PRADHAN <bikashp2018@gmail.com>
2023-11-09 18:14:53 +0530

    demo

---
+++ TypeCast/TypeCast/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "bikash";
+            Console.WriteLine(name);
+            Console.Read();
+            //Console.ReadLine();
+            
+            /*int a = 20;
+            Console.WriteLine(a);
+            string z;
+            //z= a.ToString();
+            Console.WriteLine(z);
+            Console.ReadKey();*/
+        }
+    }
+}
