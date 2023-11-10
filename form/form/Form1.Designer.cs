defeb45159c530841a8a0b6de24a1cf2a58018b6
BIKASH PRADHAN <bikashp2018@gmail.com>
2023-11-10 23:19:10 +0530

    formbutton

--- form/form/Form1.Designer.cs
+++ form/form/Form1.Designer.cs
@@ -29,35 +29,53 @@
         private void InitializeComponent()
         {
             this.Submit = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // Submit
             // 
+            this.Submit.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Submit.Location = new System.Drawing.Point(329, 184);
+            this.Submit.Location = new System.Drawing.Point(200, 89);
             this.Submit.Name = "Submit";
             this.Submit.Size = new System.Drawing.Size(121, 32);
-            this.Submit.TabIndex = 0;
+            this.Submit.TabIndex = 1;
             this.Submit.Text = "Submit";
-            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            this.Submit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Submit_KeyPress);
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(329, 138);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(251, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(432, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox2
+            // button1
             // 
-            this.textBox2.Location = new System.Drawing.Point(329, 91);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(251, 20);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.button1.Location = new System.Drawing.Point(552, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Label control";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(166, 168);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // mainform
             // 
@@ -68,8 +86,9 @@
             this.BackgroundImage = global::form.Properties.Resources._7_wonders_eco_park_9__;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Submit);
             this.Name = "mainform";
             this.Text = "Login From";
@@ -82,8 +101,9 @@
         #endregion
 
         private System.Windows.Forms.Button Submit;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- form/form/Form1.cs
+++ form/form/Form1.cs
@@ -19,7 +19,15 @@ namespace form
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            String messagetext = "Do you want to close window";
+           Submit.Text = "Login";
+            //combo box
+           /* comboBox1.Items.Add("bikash");
+            comboBox1.Items.Add("biku");
+            comboBox1.Items.Add("bi");
+            comboBox1.Items.Add("Pradhan");
+            comboBox1.SelectedIndex = 1;*/
+           //message box
+           /* String messagetext = "Do you want to close window";
             String messagetitle = "Close Window";
             MessageBoxButtons buttons = MessageBoxButtons.YesNo;
             //MessageBox.Show("simple message text");
@@ -33,14 +41,31 @@ namespace form
             {
                 // DO nothing
             }
+           */
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void Submit_Click(object sender, EventArgs e)
         {
-
+            //MessageBox.Show("Hello");
+            Submit.Text = "Clicked";
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void Submit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Submit.Text = "Enter";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "First Label"; 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
