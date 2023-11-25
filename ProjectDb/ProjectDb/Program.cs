68b1ddef54711e3773597f2b6da84f3026adfd7a
BIKASH PRADHAN <bikashp2018@gmail.com>
2023-11-25 18:29:30 +0530

    project

---
+++ ProjectDb/ProjectDb/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+using System.Data;
+
+namespace ProjectDb
+{
+    public  class Program
+    {
+        private string connectionString = "your_connection_string_here";
+
+        public void CreateTable()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "CREATE TABLE ExampleTable (ID INT PRIMARY KEY, Name NVARCHAR(50))";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void InsertData(int id, string name)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO ExampleTable (ID, Name) VALUES (@ID, @Name)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ID", id);
+                    command.Parameters.AddWithValue("@Name", name);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public DataTable RetrieveData()
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM ExampleTable";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+
+            return dataTable;
+        }
+
+        public void DeleteData(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "DELETE FROM ExampleTable WHERE ID = @ID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ID", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program sqlOperations = new Program();
+
+            // Create table
+            sqlOperations.CreateTable();
+
+            // Insert data
+            sqlOperations.InsertData(1, "John Doe");
+
+            // Retrieve data
+            DataTable data = sqlOperations.RetrieveData();
+            Console.WriteLine("Data Retrieved:");
+            foreach (DataRow row in data.Rows)
+            {
+                Console.WriteLine($"ID: {row["ID"]}, Name: {row["Name"]}");
+            }
+
+            // Delete data
+            sqlOperations.DeleteData(1);
+
+            // Retrieve data after deletion
+            data = sqlOperations.RetrieveData();
+            Console.WriteLine("Data After Deletion:");
+            foreach (DataRow row in data.Rows)
+            {
+                Console.WriteLine($"ID: {row["ID"]}, Name: {row["Name"]}");
+            }
+        }
+       
+    }
+}
---
+++ ProjectDbForm/ProjectDbForm/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjectDbForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=MyTestDB;Integrated Security=True;Pooling=False");
+           con.Open();
+            SqlCommand cmd = new SqlCommand("insert into UserTab values(@ID,@Name,@age)", con);
+            cmd.Parameters.AddWithValue("@ID",int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name",(textBox2.Text));
+            cmd.Parameters.AddWithValue("@Age",int.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Saved");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=MyTestDB;Integrated Security=True;Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update UserTab set Name = @Name,Age=@Age where ID = @ID", con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", (textBox2.Text));
+            cmd.Parameters.AddWithValue("@Age", int.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Updated");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text != "") { 
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=MyTestDB;Integrated Security=True;Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete UserTab where Name=@Name", con);
+            cmd.Parameters.AddWithValue("@Name", (textBox2.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Deleted");
+            }
+            else
+            {
+                MessageBox.Show("Please enter name only you want to Delete");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=MyTestDB;Integrated Security=True;Pooling=False");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from UserTab where Name=@Name", con);
+                cmd.Parameters.AddWithValue("@Name", (textBox2.Text));
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("Enter the name only you want to Search");
+            }
+
+        }
+    }
+}
