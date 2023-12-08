40074f90e403e850fa0cdcdc8f26bd63e9de1a8a
BIKASH PRADHAN <bikashp2018@gmail.com>
2023-12-09 02:25:37 +0530

    nkn

--- FirstProjectMvc/FirstProjectMvc/Controllers/EmployeeController.cs
+++ FirstProjectMvc/FirstProjectMvc/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FirstProjectMvc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,14 +12,41 @@ namespace FirstProjectMvc.Controllers
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
+            List<Employee> emplist = new List<Employee>();
+            Employee employee = new Employee();
+            employee.Id = 1;
+            employee.Name = "Bikash";
+            employee.Salary = 8000;
+            emplist.Add(employee);
+            Employee emp2 = new Employee();
+            emp2.Id = 2;
+            emp2.Name = "Prakash";
+            emp2.Salary = 90000;
+            emplist.Add(emp2);
+            return View(emplist);
         }
         public ActionResult AddEmployee()
         {
-            return View();
-        } public ActionResult UpdateEmployee()
+            Employee emp = new Employee();
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Employee employee)
         {
             return View();
+        }
+        public ActionResult UpdateEmployee()
+        {
+            Employee employee = new Employee();
+            employee.Id = 1;
+            employee.Name = "biku";
+            employee.Salary = 50000;
+            return View(employee);
+        }
+        [HttpPost]
+        public ActionResult UpdateEmployee(Employee employee)
+        {
+            return View(employee);
         }
     }
 }
